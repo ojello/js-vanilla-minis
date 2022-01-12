4f7c2007f72856d290043fdb92cb60247398043e
Kist <kbear.swe@gmail.com>
2022-01-12 16:56:37 -0600

    WIP

---
+++ squadcard/app.js
@@ -0,0 +1,49 @@
+// Creating object with people info
+let data = [
+    {
+        name: "Amber",
+        age: '21',
+    }
+    ,
+    {
+        name: "Adi",
+        age: '24',
+    }
+    ,
+    {
+        name: "Blake",
+        age: '25',
+    }
+    ,
+    {
+        name: "Kat",
+        age: '20',
+    }
+    ,
+    {
+        name: "Steven",
+        age: '27',
+    }
+    ,
+    {
+        name: "Alex",
+        age: '28',
+    }
+];
+
+
+//  function details that returns open div and people name, age,  closes with div
+// function goes through the array data and displays it
+
+
+// selecting id Info
+const info = document.querySelector('#info');
+
+document.querySelector('#info');
+
+let detail = data.map(function(item){
+    return '<div>' + item.name + ' '+ ' is' + item.age + ' years old' + '</div>';
+});
+
+
+
