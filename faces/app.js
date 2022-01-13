675bc5ac6d78825f015ddc8be54cdc576522153a
Kist <kbear.swe@gmail.com>
2022-01-13 09:39:56 -0600

    update

--- faces/app.js
+++ faces/app.js
@@ -2,15 +2,15 @@ const wink = document.querySelector('.closed');
 const coy = document.querySelector('.open');
 
 // Adding eventlistener w/ arrow function
-wink.addEventListener('click', () =>{
-    if(coy.classList.contains('open')) {
+wink.addEventListener('click', () => {
+    if (coy.classList.contains('open')) {
         // displays the open coy face now 
         coy.classList.add('active');
         wink.classList.remove('active');
     }
 })
-coy.addEventListener('click', () =>{
-    if(wink.classList.contains('closed')){
+coy.addEventListener('click', () => {
+    if (wink.classList.contains('closed')) {
         wink.classList.add('active');
         coy.classList.remove('active');
     }
