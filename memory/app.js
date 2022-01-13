0e2ac49f0427124e0f9783eab1cda829e27c50ac
Kist <kbear.swe@gmail.com>
2022-01-13 12:16:05 -0600

    update

---
+++ memory/app.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+// Card options
+// card Array 
+const cardArray = [
+    {
+        name: 'fish',
+        img: 'memory/images/fish.png'
+    },
+    {
+        name: 'icecream',
+        img: 'memory/images/icecream.jpeg'
+    },
+    {
+        name: 'lift',
+        img: 'memory/images/lift.jpeg'
+    },
+    {
+        name: 'lolipop',
+        img: 'memory/images/lolipop.png'
+    },
+    {
+        name: 'peach',
+        img: 'memory/images/peach.jpeg'
+    },
+    {
+        name: 'squish',
+        img: 'memory/images/squish.jpeg'
+    },
+    {
+        name: 'uwu',
+        img: 'memory/images/uwu.png'
+    },
+    {
+        name: 'wfish',
+        img: '/Users/kistb/Documents/js-vanilla-minis/memory/images/wfish.jpeg'
+    },
+]
+
+const grid = document.querySelector('.grid')
+// create your board
+// use for loop to loop through each element and create a card
+
+function createBoard() {
+for (let i = 0; i < cardArray.length; i++){
+    var card = document.createElement('img')
+    // linking it to relative path image 'blank'
+    card.setAttribute('src', 'memory/images/empty.png')
+    // give each image a data-id
+    card.setAttribute('data-id', i)
+    // card.addEventListener('click', flipcard)
+    grid.appendChild(card)
+}
+}
+
+createBoard()
+})
