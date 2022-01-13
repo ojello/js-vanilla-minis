8d413fa01f4958df467a8787e7050ab358299222
Kist <kbear.swe@gmail.com>
2022-01-13 15:30:12 -0600

    update

--- memory/app.js
+++ memory/app.js
@@ -70,6 +70,8 @@ const cardArray = [
     
 ]
 
+
+
 cardArray.sort(()=> .5 - Math.random())
 
 const grid = document.querySelector('.grid')
@@ -97,14 +99,24 @@ for (let i = 0; i < cardArray.length; i++){
 
 // check for matches
 function checkForMatch() {
-    var cards = document.querySelectorAll('img')
+    const cards = document.querySelectorAll('img')
     const optionOneId = cardsChosenId[0]
     const optionTwoId = cardsChosenId[1]
-    if (cardsChosen[0] === cardsChosen[1]){
+  
+    if(optionOneId == optionTwoId) {
+        cards[optionOneId].setAttribute('src','../memory/images/fpastel.png')
+
+        cards[optionTwoId].setAttribute('src','../memory/images/fpastel.png')
+        alert('Clicked the image twice!')
+    }
+    else if (cardsChosen[0] === cardsChosen[1]){
         // if cards match
         alert('you found a match')
         cards[optionOneId].setAttribute('src', '../memory/images/empty.png')
         cards[optionTwoId].setAttribute('src', '../memory/images/empty.png')
+// remove the click function of the matched pair
+        cards[optionOneId].removeEventListener("click",flipCard);
+        cards[optionTwoId].removeEventListener("click", flipCard);
         cardsWon.push(cardsChosen)
     // if no match
     } else{
@@ -120,17 +132,17 @@ function checkForMatch() {
     }
 }
 
-
+// flipping the card
 function flipCard() {
     // retrieving the data attribute
-    var cardId = this.getAttribute('data-id')
+    let cardId = this.getAttribute('data-id')
     cardsChosen.push(cardArray[cardId].name)
     cardsChosenId.push(cardId)
     // the setAttribute will 
     // let add an image to that square based on the cardId it holds
 
     this.setAttribute('src', cardArray[cardId].img)
-    if (cardsChosen.length == 2) {
+    if (cardsChosen.length === 2) {
         // allows the choosing of two cards
         // and gives the player 500 miliseconds to do so.
         setTimeout(checkForMatch, 500)
@@ -139,5 +151,4 @@ function flipCard() {
 
 // calls the creation of the board
 createBoard()
-
 })
